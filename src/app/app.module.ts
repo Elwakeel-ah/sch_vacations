9bffb146316dab0fe88190b064b57921efcaf246
Ahmed Hassan <elwakeel.ah@gmail.com>
2023-06-26 21:29:07 +0300

    initial commit - preliminary layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VacationTypeComponent } from './vacation-type/vacation-type.component';
+import { CreateRequestComponent } from './create-request/create-request.component';
+import { VacationBalanceComponent } from './vacation-balance/vacation-balance.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VacationTypeComponent,
+    CreateRequestComponent,
+    VacationBalanceComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/vacation-balance/vacation-balance.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vacation-balance',
+  templateUrl: './vacation-balance.component.html',
+  styleUrls: ['./vacation-balance.component.css']
+})
+export class VacationBalanceComponent {
+
+}
